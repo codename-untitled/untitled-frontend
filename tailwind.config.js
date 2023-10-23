8f044f10795e1f39be5ccb27117e029c7be206eb
qgatssdev <chivoomodu@gmail.com>
2023-10-23 23:54:38 +0100

    config tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {},
+    colors: {
+      black: '#120808',
+      orange: '#FFFBF5',
+    },
+  },
+  plugins: [],
+};
