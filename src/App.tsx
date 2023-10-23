a2aad91a355a345a7471894b6b5ac96cd9c13726
qgatssdev <chivoomodu@gmail.com>
2023-10-23 23:56:15 +0100

    feat: initialise routing with react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
-import React from 'react';
-import './App.css';
+import ScrollToTop from 'helpers/scroll-to-top';
+import { Routes, Route } from 'react-router-dom';
+import Home from 'pages/home';
+import Company from 'pages/company';
+import Analytics from 'pages/company/analytics';
+import CompanyEmployee from 'pages/company/employee';
+import Repository from 'pages/company/repository';
+import Workflow from 'pages/company/workflow';
 
 function App() {
   return (
-    <div className="App">
-     <p>Hello!</p>
-    </div>
+    <ScrollToTop>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/company' element={<Company />}>
+          <Route index element={<Analytics />} />
+          <Route path='repository' element={<Repository />} />
+          <Route path='employee' element={<CompanyEmployee />} />
+          <Route path='workflow' element={<Workflow />} />
+        </Route>
+        <Route
+          path={'/*'}
+          element={<h1 className='text-center mt-4'>404: Not Found</h1>}
+        />
+      </Routes>
+    </ScrollToTop>
   );
 }
 
