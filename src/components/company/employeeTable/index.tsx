f33b4352f89ca98d7643e110b21da70ef69b15ff
qgatssdev <chivoomodu@gmail.com>
2023-11-01 15:39:10 +0100

    feat: implement employee table

---
+++ src/components/company/employeeTable/index.tsx
@@ -0,0 +1,41 @@
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import { EmployeeList } from 'store/company/employee';
+
+
+const EmployeeTable = ({employees}: EmployeeList) => {
+  return (
+    <TableContainer>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell align='center'>First name</TableCell>
+            <TableCell align='center'>Last name</TableCell>
+            <TableCell align='center'>Email</TableCell>
+            <TableCell align='center'>Role</TableCell>
+            <TableCell align='center'>Department</TableCell>
+            <TableCell align='center'>Progress</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {employees.map((employee, index) => (
+            <TableRow key={index}>
+              <TableCell align='center'>{employee.firstName}</TableCell>
+              <TableCell align='center'>{employee.lastName}</TableCell>
+              <TableCell align='center'>{employee.email}</TableCell>
+              <TableCell align='center'>{employee.role}</TableCell>
+              <TableCell align='center'>{employee.department}</TableCell>
+              <TableCell align='center'>{employee.progress}%</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
+
+export default EmployeeTable;
--- src/pages/company/employee/index.tsx
+++ src/pages/company/employee/index.tsx
@@ -1,7 +1,23 @@
+import AddButton from 'components/company/buttons/addButton';
+import FilterButton from 'components/company/buttons/filterButton';
+import EmployeeTable from 'components/company/employeeTable';
+import EmptyState from 'components/company/emptyState';
+import { employees } from 'mockdata/employees';
+
 const CompanyEmployee = () => {
   return (
-    <div>
-      <p className='text-center'>Company employee</p>
+    <div className='mx-[5%]'>
+      <div className='flex justify-between mt-[30px]'>
+        <FilterButton />
+        <AddButton name='Add Employee' />
+      </div>
+      <div className='mt-[30px] rounded-md bg-white h-[100%] min-h-[550px] shadow-[1px_1px_0px_0px_#000] border-solid border'>
+        {employees.length === 0 ? (
+          <EmptyState placeholder='No employee found' />
+        ) : (
+          <EmployeeTable employees={employees} />
+        )}
+      </div>
     </div>
   );
 };
