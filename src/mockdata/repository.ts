f73807f37ccce192eed46d85dd879de02219b98b
qgatssdev <chivoomodu@gmail.com>
2023-11-04 23:48:22 +0100

    chore: mockdata

---
+++ src/mockdata/repository.ts
@@ -0,0 +1,77 @@
+export const repositoryFiles = [
+    {
+        name: 'Annual Report 2022',
+        documentType: 'PDF',
+        date: 'January 1, 2023'
+    },
+    {
+        name: 'Invoice_Customer12345',
+        documentType: 'PDF',
+        date: 'January 15, 2023'
+    },
+    {
+        name: 'Employee Contract XYZ',
+        documentType: 'DOC',
+        date: 'February 10, 2023'
+    },
+    {
+        name: 'Jane Doe Resume',
+        documentType: 'PDF',
+        date: 'March 5, 2023'
+    },
+    {
+        name: 'Project Proposal ABC',
+        documentType: 'DOCX',
+        date: 'April 20, 2023'
+    },
+    {
+        name: 'Order Receipt 56789',
+        documentType: 'PDF',
+        date: 'May 12, 2023'
+    },
+    {
+        name: 'Corporate Policy 123',
+        documentType: 'PDF',
+        date: 'June 30, 2023'
+    },
+    {
+        name: 'Vendor Contract XYZ',
+        documentType: 'DOC',
+        date: 'July 22, 2023'
+    },
+    {
+        name: 'Partnership Agreement ABC',
+        documentType: 'PDF',
+        date: 'August 14, 2023'
+    },
+    {
+        name: 'Project Proposal XYZ',
+        documentType: 'DOCX',
+        date: 'September 5, 2023'
+    },
+    {
+        name: 'John Smith Resume',
+        documentType: 'PDF',
+        date: 'October 18, 2023'
+    },
+    {
+        name: 'Invoice_Customer67890',
+        documentType: 'PDF',
+        date: 'November 9, 2023'
+    },
+    {
+        name: 'Quarterly Report Q3 2023',
+        documentType: 'PDF',
+        date: 'December 31, 2023'
+    },
+    {
+        name: 'Supplier Agreement 123',
+        documentType: 'PDF',
+        date: 'January 15, 2024'
+    },
+    {
+        name: 'Order Receipt 12345',
+        documentType: 'PDF',
+        date: 'February 10, 2024'
+    }
+];
