db0629c32258b91bdeac98c7367b11e586608825
qgatssdev <chivoomodu@gmail.com>
2023-11-06 14:20:01 +0100

    chore: new mockdata

---
+++ src/mockdata/workflow.ts
@@ -0,0 +1,40 @@
+const workflowTemplates = [
+  {
+    id: 1,
+    name: 'Workflow1',
+  },
+  {
+    id: 2,
+    name: 'Software Engineers',
+  },
+  {
+    id: 3,
+    name: 'Designers',
+  },
+  {
+    id: 4,
+    name: 'Writers',
+  },
+  {
+    id: 5,
+    name: 'HR',
+  },
+  {
+    id: 7,
+    name: 'Dancers',
+  },
+  {
+    id: 8,
+    name: 'Managers',
+  },
+  {
+    id: 9,
+    name: 'Recruiters',
+  },
+  {
+    id: 10,
+    name: 'Analyst',
+  },
+];
+
+export default workflowTemplates;
